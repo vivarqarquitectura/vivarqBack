88bb85651cbb934e39472b2f22e18eb3008dee67
IvanoffFrancisco <nicoo.ivanoff@gmail.com>
2025-10-07 11:31:44 -0300

    se agrega funciones y router proyecto favorito

--- src/controllers/proyecto_favorito/favoritos.controller.js
+++ src/controllers/proyecto_favorito/favoritos.controller.js
@@ -4,7 +4,7 @@ import {pool} from '../../conexionDB.js';
 export const obtenerFavoritos=async (req,res)=>{
     const {id_usuario}=req.body;
     try {
-         const [resultado]=await pool.query('SELECT * FROM `proyecto_favorito` WHERE id_usuario=?',[id_usuario]);  
+         const [resultado]=await pool.query('SELECT proyecto.id_proyecto,proyecto.nombre,proyecto.estilo,proyecto.img_proyecto,proyecto.frente, proyecto.fondo,proyecto.cocina,proyecto.baño,proyecto.habitacion,proyecto.sala_estar FROM proyecto_favorito INNER JOIN proyecto on proyecto_favorito.id_proyecto=proyecto.id_proyecto WHERE id_usuario=?',[id_usuario]);  
              res.status(200).json(resultado)
 
 
@@ -23,6 +23,19 @@ export const agregarFavoritos=async (req,res)=>{
 
      } catch (error) {
         console.error("Error en obtener un proyecto favorito: ", error);
+        res.status(500).json({ error: "Error en el servidor" });
+    }
+}
+
+export const eliminarFavorito= async(req,res)=>{
+    const {id_proyecto, id_usuario}=req.body;
+    try {
+         const [resultado]=await pool.query('DELETE FROM proyecto_favorito WHERE id_usuario=? && id_proyecto=?',[id_usuario,id_proyecto ]);  
+             res.status(200).json(resultado)
+
+
+     } catch (error) {
+        console.error("Error al intentar borrar un proyecto favorito: ", error);
         res.status(500).json({ error: "Error en el servidor" });
     }
 }
--- src/routes/proyecto_favorito/favorito.route.js
+++ src/routes/proyecto_favorito/favorito.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import {agregarFavoritos,obtenerFavoritos} from '../../controllers/proyecto_favorito/favoritos.controller.js';
+import {agregarFavoritos,obtenerFavoritos,eliminarFavorito} from '../../controllers/proyecto_favorito/favoritos.controller.js';
 const router=Router();
 
 router.post('/obtenerFavoritos', obtenerFavoritos);
 
 router.post('/nuevoFavorito', agregarFavoritos);
+
+router.delete('/eliminarFavorito',eliminarFavorito);
 
 
 export default router;
